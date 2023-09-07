0100bd0e29b97321a1c38f5d6a8f19bb2cade3e0
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-09-08 03:29:16 +0900

    feat: 정렬 및 strike 기능 구현

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Editor } from 'https://esm.sh/@tiptap/core';
 import StarterKit from 'https://esm.sh/@tiptap/starter-kit';
 import Image from 'https://esm.sh/@tiptap/extension-image';
+import TextAlign from 'https://esm.sh/@tiptap/extension-text-align';
 
 (function () {
   const defaultContent = `
@@ -34,13 +35,24 @@ import Image from 'https://esm.sh/@tiptap/extension-image';
     italic: document.querySelector('[data-tiptap-button="italic"]'),
     image: document.querySelector('[data-tiptap-button="image"]'),
     heading2: document.querySelector('[data-tiptap-button="heading2"]'),
+    heading3: document.querySelector('[data-tiptap-button="heading3"]'),
+    paragraph: document.querySelector('[data-tiptap-button="paragraph"]'),
+    strike: document.querySelector('[data-tiptap-button="strike"]'),
+    left: document.querySelector('[data-tiptap-button="left"]'),
+    center: document.querySelector('[data-tiptap-button="center"]'),
   };
 
   const content = document.querySelector('[data-tiptap-content]');
 
   const editor = new Editor({
     element: document.querySelector('[data-tiptap-editor]'),
-    extensions: [StarterKit, Image],
+    extensions: [
+      StarterKit,
+      Image,
+      TextAlign.configure({
+        types: ['heading', 'paragraph'],
+      }),
+    ],
     content: defaultContent,
     onUpdate({ editor }) {
       // content.innerHTML = JSON.stringify(editor.getJSON());
@@ -49,12 +61,22 @@ import Image from 'https://esm.sh/@tiptap/extension-image';
       buttons.bold.classList.toggle('active', editor.isActive('bold'));
       buttons.italic.classList.toggle('active', editor.isActive('italic'));
       buttons.heading2.classList.toggle('active', editor.isActive('heading', { level: 2 }));
+      buttons.heading3.classList.toggle('active', editor.isActive('heading', { level: 3 }));
+      buttons.paragraph.classList.toggle('active', editor.isActive('paragraph'));
+      buttons.strike.classList.toggle('active', editor.isActive('strike'));
+      buttons.left.classList.toggle('active', editor.isActive({ textAlign: 'left' }));
+      buttons.center.classList.toggle('active', editor.isActive({ textAlign: 'center' }));
     },
     onSelectionUpdate({ editor }) {
       console.log('selection update');
       buttons.bold.classList.toggle('active', editor.isActive('bold'));
       buttons.italic.classList.toggle('active', editor.isActive('italic'));
       buttons.heading2.classList.toggle('active', editor.isActive('heading', { level: 2 }));
+      buttons.heading3.classList.toggle('active', editor.isActive('heading', { level: 3 }));
+      buttons.paragraph.classList.toggle('active', editor.isActive('paragraph'));
+      buttons.strike.classList.toggle('active', editor.isActive('strike'));
+      buttons.left.classList.toggle('active', editor.isActive({ textAlign: 'left' }));
+      buttons.center.classList.toggle('active', editor.isActive({ textAlign: 'center' }));
     },
     onCreate({ editor }) {
       console.log(editor.getHTML());
@@ -78,7 +100,26 @@ import Image from 'https://esm.sh/@tiptap/extension-image';
   });
 
   buttons.heading2.addEventListener('click', () => {
-    // editor.isActive('heading', { level: 2 });
     editor.chain().focus().toggleHeading({ level: 2 }).run();
+  });
+
+  buttons.heading3.addEventListener('click', () => {
+    editor.chain().focus().toggleHeading({ level: 3 }).run();
+  });
+
+  buttons.paragraph.addEventListener('click', () => {
+    editor.chain().focus().setParagraph().run();
+  });
+
+  buttons.strike.addEventListener('click', () => {
+    editor.chain().focus().toggleStrike().run();
+  });
+
+  buttons.left.addEventListener('click', () => {
+    editor.chain().focus().setTextAlign('left').run();
+  });
+
+  buttons.center.addEventListener('click', () => {
+    editor.chain().focus().setTextAlign('center').run();
   });
 })();
