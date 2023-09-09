949f91a68fd049f8543d30c99e2e31a3f2792ce4
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-09-09 22:55:12 +0900

    design: editor css 수정

--- index.js
+++ index.js
@@ -4,32 +4,6 @@ import Image from 'https://esm.sh/@tiptap/extension-image';
 import TextAlign from 'https://esm.sh/@tiptap/extension-text-align';
 
 (function () {
-  const defaultContent = `
-            <h1>HTML Ipsum Presentsㅇㅇ</h1>
-
-            <img src="https://source.unsplash.com/8xznAGy4HcY/800x400" />
-            <img src="https://source.unsplash.com/K9QHL52rE2k/800x400" />
-
-            <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
-
-            <h2>Header Level 2</h2>
-
-            <ol>
-              <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-              <li>Aliquam tincidunt mauris eu risus.</li>
-            </ol>
-
-            <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
-
-            <h3>Header Level 3</h3>
-
-            <ul>
-              <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-              <li>Aliquam tincidunt mauris eu risus.</li>
-            </ul>
-
-        `;
-
   const buttons = {
     bold: document.querySelector('[data-tiptap-button="bold"]'),
     italic: document.querySelector('[data-tiptap-button="italic"]'),
@@ -43,8 +17,6 @@ import TextAlign from 'https://esm.sh/@tiptap/extension-text-align';
     right: document.querySelector('[data-tiptap-button="right"]'),
   };
 
-  const content = document.querySelector('[data-tiptap-content]');
-
   const editor = new Editor({
     element: document.querySelector('[data-tiptap-editor]'),
     extensions: [
@@ -54,16 +26,16 @@ import TextAlign from 'https://esm.sh/@tiptap/extension-text-align';
         types: ['heading', 'paragraph'],
       }),
     ],
-    content: defaultContent,
+    content: '',
     onUpdate({ editor }) {
       // content.innerHTML = JSON.stringify(editor.getJSON());
       console.log(editor.getHTML());
 
-      buttons.bold.classList.toggle('active', editor.isActive('bold'));
-      buttons.italic.classList.toggle('active', editor.isActive('italic'));
       buttons.heading2.classList.toggle('active', editor.isActive('heading', { level: 2 }));
       buttons.heading3.classList.toggle('active', editor.isActive('heading', { level: 3 }));
       buttons.paragraph.classList.toggle('active', editor.isActive('paragraph'));
+      buttons.bold.classList.toggle('active', editor.isActive('bold'));
+      buttons.italic.classList.toggle('active', editor.isActive('italic'));
       buttons.strike.classList.toggle('active', editor.isActive('strike'));
       buttons.left.classList.toggle('active', editor.isActive({ textAlign: 'left' }));
       buttons.center.classList.toggle('active', editor.isActive({ textAlign: 'center' }));
@@ -71,11 +43,11 @@ import TextAlign from 'https://esm.sh/@tiptap/extension-text-align';
     },
     onSelectionUpdate({ editor }) {
       console.log('selection update');
-      buttons.bold.classList.toggle('active', editor.isActive('bold'));
-      buttons.italic.classList.toggle('active', editor.isActive('italic'));
       buttons.heading2.classList.toggle('active', editor.isActive('heading', { level: 2 }));
       buttons.heading3.classList.toggle('active', editor.isActive('heading', { level: 3 }));
       buttons.paragraph.classList.toggle('active', editor.isActive('paragraph'));
+      buttons.bold.classList.toggle('active', editor.isActive('bold'));
+      buttons.italic.classList.toggle('active', editor.isActive('italic'));
       buttons.strike.classList.toggle('active', editor.isActive('strike'));
       buttons.left.classList.toggle('active', editor.isActive({ textAlign: 'left' }));
       buttons.center.classList.toggle('active', editor.isActive({ textAlign: 'center' }));
@@ -86,21 +58,6 @@ import TextAlign from 'https://esm.sh/@tiptap/extension-text-align';
       // content.innerHTML = JSON.stringify(editor.getJSON());
     },
   }); // add your configuration, extensions, content, etc.
-
-  buttons.bold.addEventListener('click', () => {
-    editor.chain().focus().toggleBold().run();
-  });
-
-  buttons.italic.addEventListener('click', () => {
-    editor.chain().focus().toggleItalic().run();
-  });
-
-  buttons.image.addEventListener('click', () => {
-    const url = window.prompt('URL');
-    if (url) {
-      editor.chain().focus().setImage({ src: url }).run();
-    }
-  });
 
   buttons.heading2.addEventListener('click', () => {
     editor.chain().focus().toggleHeading({ level: 2 }).run();
@@ -114,7 +71,18 @@ import TextAlign from 'https://esm.sh/@tiptap/extension-text-align';
     editor.chain().focus().setParagraph().run();
   });
 
+  buttons.bold.addEventListener('click', () => {
+    buttons.bold.classList.toggle('active');
+    editor.chain().focus().toggleBold().run();
+  });
+
+  buttons.italic.addEventListener('click', () => {
+    buttons.italic.classList.toggle('active');
+    editor.chain().focus().toggleItalic().run();
+  });
+
   buttons.strike.addEventListener('click', () => {
+    buttons.strike.classList.toggle('active');
     editor.chain().focus().toggleStrike().run();
   });
 
@@ -128,5 +96,12 @@ import TextAlign from 'https://esm.sh/@tiptap/extension-text-align';
 
   buttons.right.addEventListener('click', () => {
     editor.chain().focus().setTextAlign('right').run();
+  });
+
+  buttons.image.addEventListener('click', () => {
+    const url = window.prompt('URL');
+    if (url) {
+      editor.chain().focus().setImage({ src: url }).run();
+    }
   });
 })();
